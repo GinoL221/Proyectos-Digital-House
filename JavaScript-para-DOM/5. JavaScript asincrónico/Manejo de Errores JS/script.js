3e53c2b027e658944aa143a070049524cb8ea4f6
Gino <ginolencina@gmail.com>
2024-11-05 21:10:12 -0300

    act

---
+++ JavaScript-para-DOM/5. JavaScript asincrónico/Manejo de Errores JS/script.js
@@ -0,0 +1,26 @@
+// TRY (intentar), CATCH (capturar), FINALLY (finalmente), THROW (lanzar)
+/*
+try {
+  unaFuncion("Hola Mundo!!");
+} catch (error) {
+  console.log(error.message);
+  document.getElementById("error").innerHTML = error.message;
+}
+*/
+
+const testNumber = () => {
+  const message = document.getElementById("res"); //Seleccionamos el párrafo
+  message.innerHTML = ""; //Vaciamos el parrafo
+
+  let num = document.getElementById("input").value; //Seleccionar el valor del input (string)
+  try {
+    if (num.trim() == "") throw "vacío";
+    if (isNaN(num)) throw "no es un número";
+    num = Number(num); //Convertimos el string a número
+    if (num < 1) throw "muy bajo";
+    if (num > 10) throw "muy alto";
+    message.innerHTML = num;
+  } catch (error) {
+    message.innerHTML = "El dato ingresado es: " + error;
+  }
+};
