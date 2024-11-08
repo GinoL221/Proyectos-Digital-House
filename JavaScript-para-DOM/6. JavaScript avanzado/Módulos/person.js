92016bd36bab3e51aaf5bee98c645e692315f08e
Gino <ginolencina@gmail.com>
2024-11-08 15:09:20 -0300

    Agregar implementación de módulos en JavaScript con un ejemplo de uso de funciones y exportaciones

---
+++ JavaScript-para-DOM/6. JavaScript avanzado/Módulos/person.js
@@ -0,0 +1,9 @@
+const message = () => {
+  const name = "Pedro";
+  const age = 25;
+  return `${name} tiene ${age} años  de edad`;
+};
+
+export const otraVariable = "Maria tiene 30 años";
+
+export default message;
---
+++ JavaScript-para-DOM/6. JavaScript avanzado/Módulos/script.js
@@ -0,0 +1,5 @@
+import myMessage from "./person.js";
+import { otraVariable } from "./person.js";
+
+document.getElementById("message").innerHTML = myMessage();
+document.getElementById("message2").innerHTML = otraVariable;
