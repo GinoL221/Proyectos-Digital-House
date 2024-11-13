31aa501ed43c8cd43152bec584ede6aabeca7f0d
Gino <ginolencina@gmail.com>
2024-11-13 19:08:03 -0300

    Agregar estilos CSS y actualizar componente VariablesApp para incluir encabezados y párrafos con diferentes estilos

--- React/Primeros pasos en React/primer-proyecto-react/src/VariablesApp.jsx
+++ React/Primeros pasos en React/primer-proyecto-react/src/VariablesApp.jsx
@@ -1,3 +1,4 @@
+/*
 const string = 'Curso de React' // Se ve tal cual el string sin comillas
 const string2 = "Digital House" // Se ve tal cual el string sin comillas
 const string3 = `Este es un ${string} que estás cursando en ${string2}` // Se ve una cadena de texto
@@ -8,11 +9,33 @@ const foo = () => 'Devuelve un string' // Si no la ejecuto rompe la app, debo ej
 const object = { name: 'Sergie', job: 'Programador' } // Esto no funciona y rompe la app ¿Como veo un objeto?
 const set = new Set([1,2,3,4]) // Es una excepción ya que aunque sea un objeto, lo mismo muestra el derivado ya sea string o array
 const date = new Date() // Esto no funciona y rompe la app ¿Como veo un fecha?
+*/
+import "./VariablesApp.css";
 
 export const VariablesApp = () => {
-    return (
-    <div>
-        {JSON. stringify(array)}
-    </div>
-    )
-}
+  const style = {
+    color: "blue",
+    fontSize: "20px",
+  };
+  return (
+    <>
+      <h1>Esta clase es de estilos</h1>
+      <h2 id="subtitulo">Vamos a utilizar distintos selectores</h2>
+      <h4 className="red">Este es un titulo rojo</h4>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis,
+          quibusdam consectetur blanditiis modi incidunt recusandae nulla nihil
+          ullam distinctio soluta sequi! Aliquam in veritatis aliquid laborum
+          accusantium quia quis modi ?
+        </p>
+      </div>
+      <p style={style}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis,
+        quibusdam consectetur blanditiis modi incidunt recusandae nulla nihil
+        ullam distinctio soluta sequi! Aliquam in veritatis aliquid laborum
+        accusantium quia quis modi ?
+      </p>
+    </>
+  );
+};
