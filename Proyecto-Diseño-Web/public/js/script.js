9f6cce51d367f90b202196c1a912d908d6282dc9
Gino <ginolencina@gmail.com>
2024-11-14 18:19:45 -0300

    Eliminar archivos innecesarios de dependencias y ajustar la estructura de archivos en el proyecto

--- Proyecto-Diseño-Web/public/js/script.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector("#menu-icon");
-const navList = document.querySelector(".navlist");
-
-menu.onclick = () => {
-  menu.classList.toggle("bx-x");
-  navList.classList.toggle("open");
-};
