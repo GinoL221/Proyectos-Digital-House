f62d8820c0b1d9bd85c407627b6824b3ae6e2a45
Gino <ginolencina@gmail.com>
2024-11-29 16:36:27 -0300

    Agregar componentes y lógica para la gestión de tareas en la aplicación React

---
+++ 7. React/Peticiones y optimización/useCounter/src/HooksApp.jsx
@@ -0,0 +1,11 @@
+import { CounterApp } from "./components/CounterApp";
+
+export const HooksApp = () => {
+  return (
+    <>
+      <h1>Aplicación de Hooks</h1>
+      <hr />
+      <CounterApp />
+    </>
+  );
+};
