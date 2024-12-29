3b26b5f476c54c34d94e9a3e07f95e152f85072c
Gino <ginolencina@gmail.com>
2024-12-28 21:57:16 -0300

    Agregar estructura básica de la aplicación con React y Vite; incluir componentes de navegación y pantallas de usuario

---
+++ 7. React/Peticiones y optimización/proyecto-navegacion-react/src/routes/HomeScreen.jsx
@@ -0,0 +1,28 @@
+import { useContext } from "react"
+import { UserContext } from "../context/UserContext"
+
+export const HomeScreen = () => {
+
+    const { user } = useContext(UserContext)
+
+    return (
+        <table className="table">
+            <thead>
+                <tr>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Tecnología</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Redes</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">{user.name}</th>
+                    <td>{user.technology}</td>
+                    <td>{user.email}</td>
+                    <td>{user.networks}</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
